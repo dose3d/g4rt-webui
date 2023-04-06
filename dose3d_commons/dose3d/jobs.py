e7377015191f9e79be4f5840b65fef957947eaf1
Michał Niedźwiecki <nkg753@gmail.com>
2023-04-06 18:10:04 +0200

    Extract Job class and move dirs and files path resolver methods to them

--- dose3d_commons/dose3d/jobs.py
+++ dose3d_commons/dose3d/jobs.py
@@ -75,7 +75,7 @@ class JobsManager:
     def get_jobs_from_queue(self):
         """
         Get list of job_id in queue sorted by create date.
-        Return: list of [job_id, ready]
+        Return: list of objects of Job class
         """
 
         jobs = []
@@ -86,10 +86,10 @@ class JobsManager:
                 job_id = os.path.splitext(base_file)[0]
                 ready_file = os.path.join(self.QUEUE_DIR, job_id + '.ready')
                 if os.path.exists(ready_file):
-                    jobs.append([job_id, True])
+                    jobs.append(Job(self, job_id, True))
                     break
                 else:
-                    jobs.append([job_id, False])
+                    jobs.append(Job(self, job_id, False))
 
         return jobs
 
@@ -105,5 +105,40 @@ class JobsManager:
         running_jobs = self.get_dirs(self.RUNNING_DIR)
         for d in running_jobs:
             job_id = os.path.basename(d)
-            jobs.append(job_id)
+            jobs.append(Job(self, job_id))
         return jobs
+
+class Job:
+    """Manage of single job"""
+
+    jm = None
+    job_id = None
+    ready = False
+
+    def __init__(self, jm, job_id, ready=False):
+        self.jm = jm
+        self.job_id = job_id
+        self.ready = ready
+
+    def get_args_file(self):
+        """Get args file of Job from QUEUE_DIR"""
+        return os.path.join(self.jm.QUEUE_DIR, self.job_id + ".args")
+
+    def get_ready_file(self):
+        """Get ready file of Job from QUEUE_DIR"""
+        return os.path.join(self.jm.QUEUE_DIR, self.job_id + ".ready")
+
+    def get_toml_file(self):
+        """Get TOML file of Job from QUEUE_DIR"""
+        return os.path.join(self.jm.QUEUE_DIR, self.job_id + ".toml")
+
+    def get_pid_file(self):
+        return os.path.join(self.get_running_job_path(), 'pid')
+
+    def get_running_job_path(self):
+        """Get path for Job in RUNNING_DIR"""
+        return os.path.join(self.jm.RUNNING_DIR, self.job_id)
+
+    def get_done_job_path(self):
+        """Get path for Job in DONE_DIR"""
+        return os.path.join(self.jm.DONE_DIR, self.job_id)
--- runner/main.py
+++ runner/main.py
@@ -9,16 +9,17 @@ import psutil
 from dose3d import JobsManager
 
 
-def execute_job(config, job_id):
+def execute_job(job):
+    job_id = job.job_id
     print('Start job %s...' % job_id)
 
     # extract job ID from job file name
-    args_file = os.path.join(config['QUEUE_DIR'], job_id + ".args")
-    ready_file = os.path.join(config['QUEUE_DIR'], job_id + ".ready")
-    job_file = os.path.join(config['QUEUE_DIR'], job_id + ".toml")
+    args_file = job.get_args_file()
+    ready_file = job.get_ready_file()
+    job_file = job.get_toml_file()
 
     # prepare PENDING/job_id path
-    job_path = os.path.join(config['RUNNING_DIR'], job_id)
+    job_path = job.get_running_job_path()
     os.makedirs(job_path)
 
     # move TODO/job_id.dat to PENDING/job_id/input.dat
@@ -34,12 +35,12 @@ def execute_job(config, job_id):
     os.remove(ready_file)
 
     # execute Dose3D: DOSE3D_EXEC -t PENDING/job_id/input.toml -f
-    exe = [config["DOSE3D_EXEC"], '-t', input_toml_fn, '-o', job_path, *args]
+    exe = [job.jm.DOSE3D_EXEC, '-t', input_toml_fn, '-o', job_path, *args]
     print('Execute: %s' % ' '.join(exe))
     p = subprocess.Popen(exe, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
     # write PID to PENDING/job_id/pid file
-    pid_fn = os.path.join(job_path, 'pid')
+    pid_fn = job.get_pid_file()
     with open(pid_fn, 'wt') as pf:
         pf.write('%d' % p.pid)
     print('PID of process: %d' % p.pid)
@@ -66,13 +67,13 @@ def execute_job(config, job_id):
             break
 
     # move PENDING/job_id to DONE/job_id
-    done_path = os.path.join(config['DONE_DIR'], job_id)
+    done_path = job.get_done_job_path()
     shutil.move(job_path, done_path)
 
 
-def clean_orphan_job(config, job_id):
-    job_path = os.path.join(config['RUNNING_DIR'], job_id)
-    done_path = os.path.join(config['DONE_DIR'], job_id)
+def clean_orphan_job(job):
+    job_path = job.get_running_job_path()
+    done_path = job.get_done_job_path()
     pid_fn = os.path.join(job_path, 'pid')
 
     # check for running Dose3D process
@@ -85,8 +86,8 @@ def clean_orphan_job(config, job_id):
                 process = psutil.Process(pid)
 
                 # check if it is Dose3D process
-                if process.name() == os.path.basename(config["DOSE3D_EXEC"]):
-                    print('Orphan job "%s" is running, wait for finish process PID %d...' % (job_id, pid))
+                if process.name() == os.path.basename(job.jm.DOSE3D_EXEC):
+                    print('Orphan job "%s" is running, wait for finish process PID %d...' % (job.job_id, pid))
                 else:
                     pass  # another process just too over the same PID
 
@@ -100,9 +101,9 @@ def clean_orphan_job(config, job_id):
         except FileNotFoundError:
             pass
 
-        print('Process %d is done, job "%s" is finished, move to DONEs' % (pid, job_id))
+        print('Process %d is done, job "%s" is finished, move to DONEs' % (pid, job.job_id))
     else:
-        print('Orphan job "%s" is finished, move to DONEs' % job_id)
+        print('Orphan job "%s" is finished, move to DONEs' % job.job_id)
 
     shutil.move(job_path, done_path)
 
@@ -120,8 +121,6 @@ def main():
         print('Error! ' + str(e), file=sys.stderr)
         exit(1)
 
-    config = jm.config
-
     print('Used config settings:')
     print('QUEUE_DIR = ' + jm.QUEUE_DIR)
     print('RUNNING_DIR = ' + jm.RUNNING_DIR)
@@ -133,7 +132,7 @@ def main():
 
     # check some config settings
     if not os.path.exists(jm.DOSE3D_EXEC):
-        print('Error! File %s not found' % config['DOSE3D_EXEC'], file=sys.stderr)
+        print('Error! File %s not found' % jm.DOSE3D_EXEC, file=sys.stderr)
         exit(1)
     if not (1 <= jm.SLEEP <= 3600):
         print('Error! SLEEP should be between 1 and 3600', file=sys.stderr)
@@ -144,8 +143,8 @@ def main():
     while True:
         # Stage 1: search for orphan running jobs and clean
         running_jobs = jm.get_running_jobs()
-        for job_id in running_jobs:
-            clean_orphan_job(config, job_id)
+        for job in running_jobs:
+            clean_orphan_job(job)
 
         # Stage 2: search for new files in QUEUE directory
         # the new job should have 2 files:
@@ -154,17 +153,17 @@ def main():
         # the second file inform the writing of new_job_id.dat is done and can be consumed by runner
         new_jobs = jm.get_jobs_from_queue()
         found_ready = None
-        for [job_id, ready] in new_jobs:
-            if ready:
-                print('Found new ready job: ' + job_id)
-                found_ready = job_id
+        for job in new_jobs:
+            if job.ready:
+                print('Found new ready job: ' + job.job_id)
+                found_ready = job
                 break
             else:
-                print('Found new job but is not ready yet: ' + job_id)
+                print('Found new job but is not ready yet: ' + job.job_id)
 
         # Stage 3: the job is ready, so move to PENDING and execute Dose3D
         if found_ready:
-            execute_job(config, found_ready)
+            execute_job(found_ready)
 
         # Stage 4: wait for
         print('.', end='')
