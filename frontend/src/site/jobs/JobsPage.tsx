d29ef717e590c7c0ae6d49e52a809aa6469b83c0
Michał Niedźwiecki <nkg753@gmail.com>
2023-06-21 16:40:33 +0200

    Unify page style

--- frontend/src/site/jobs/JobsPage.tsx
+++ frontend/src/site/jobs/JobsPage.tsx
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ErrorAlert, Page, PageHeader, Title } from '../../components/layout';
+import {
+  Card,
+  CardHeader,
+  CardHeaderMain,
+  CardsContainer,
+  ErrorAlert,
+  Margin,
+  Page,
+  Title,
+} from '../../components/layout';
 import { AddIcon, RocketLaunchIcon } from '../../components/icons';
 import JobsTable from '../../components/JobsTable';
 import Pagination from '../../components/Pagination';
@@ -19,45 +28,53 @@ export default function JobsPage() {
 
   return (
     <Page>
-      <PageHeader>
-        <Breadcrumbs breadcrumbs={JobsPageBreadcrumbs} />
-        <Title>List of jobs</Title>
-        {!!lastError && <ErrorAlert className="my-4">{formatErrorToString(lastError)}</ErrorAlert>}
+      <Margin>
+        <CardsContainer>
+          <Card>
+            <CardHeader>
+              <CardHeaderMain>
+                <Breadcrumbs breadcrumbs={JobsPageBreadcrumbs} />
+                <Title>List of jobs</Title>
+              </CardHeaderMain>
+              {!!lastError && <ErrorAlert className="my-4">{formatErrorToString(lastError)}</ErrorAlert>}
 
-        <div className="h-6">{isFetching && <progress className="progress" />}</div>
+              <div className="h-6">{isFetching && <progress className="progress" />}</div>
 
-        {!!data && (
-          <div className="block items-center sm:flex md:divide-x md:divide-gray-100">
-            <form className="mb-4 sm:mb-0 sm:pr-3" action="#" method="GET">
-              <label htmlFor="products-search" className="sr-only">
-                Search
-              </label>
-              <div className="relative mt-1 sm:w-64 xl:w-96">
-                <input
-                  type="text"
-                  name="email"
-                  id="products-search"
-                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-cyan-600 focus:ring-cyan-600 sm:text-sm"
-                  placeholder="Search for jobs"
-                  disabled
-                />
-              </div>
-            </form>
-            <div className="flex w-full items-center sm:justify-end">
-              <Link to="/jobs/create" className="btn-primary btn">
-                <AddIcon />
-                New job
-              </Link>
-            </div>
-          </div>
-        )}
-      </PageHeader>
-      {!!data && (
-        <>
-          <JobsTable isLoading={isLoading} jobs={data?.results} />
-          <Pagination controller={controller} pageSize={pageSize} setPageSize={setPageSize} />
-        </>
-      )}
+              {!!data && (
+                <div className="block items-center sm:flex md:divide-x md:divide-gray-100">
+                  <form className="mb-4 sm:mb-0 sm:pr-3" action="#" method="GET">
+                    <label htmlFor="products-search" className="sr-only">
+                      Search
+                    </label>
+                    <div className="relative mt-1 sm:w-64 xl:w-96">
+                      <input
+                        type="text"
+                        name="email"
+                        id="products-search"
+                        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-cyan-600 focus:ring-cyan-600 sm:text-sm"
+                        placeholder="Search for jobs"
+                        disabled
+                      />
+                    </div>
+                  </form>
+                  <div className="flex w-full items-center sm:justify-end">
+                    <Link to="/jobs/create" className="btn-primary btn">
+                      <AddIcon />
+                      New job
+                    </Link>
+                  </div>
+                </div>
+              )}
+            </CardHeader>
+            {!!data && (
+              <>
+                <JobsTable isLoading={isLoading} jobs={data?.results} />
+                <Pagination controller={controller} pageSize={pageSize} setPageSize={setPageSize} />
+              </>
+            )}
+          </Card>
+        </CardsContainer>
+      </Margin>
     </Page>
   );
 }
