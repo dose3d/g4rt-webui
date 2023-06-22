8b51d44de2745ec4e092d6e4b9b7ce7260a41a9b
Michał Niedźwiecki <nkg753@gmail.com>
2023-06-22 20:21:49 +0200

    Drop action from useEntity. Please use useQueryWrapper instead.

--- frontend/src/drf-crud-client/useEntity.ts
+++ frontend/src/drf-crud-client/useEntity.ts
@@ -1,6 +1,6 @@
 import { FieldValues } from 'react-hook-form';
 import { useDrfQuery, UseDrfQuery } from './useDrfQuery';
-import { ActionOptions, EntityOptions, ModelOptions } from './types';
+import { EntityOptions, ModelOptions } from './types';
 import { getEntityQueryKey } from './utils';
 
 export type UseEntity<Entity extends FieldValues = FieldValues, PK extends number | string = number | string> = Omit<
@@ -8,8 +8,7 @@ export type UseEntity<Entity extends FieldValues = FieldValues, PK extends numbe
   'queryKey'
 > &
   ModelOptions &
-  EntityOptions<PK> &
-  Partial<ActionOptions>;
+  EntityOptions<PK>;
 
 /**
  * Hook for retrieve entity from DRF backend and store in cache.
@@ -18,14 +17,11 @@ export type UseEntity<Entity extends FieldValues = FieldValues, PK extends numbe
 export function useEntity<Entity extends FieldValues = FieldValues, PK extends number | string = number | string>(
   args: UseEntity<Entity, PK>,
 ) {
-  const { queryKey, primaryKey, endpoint, action, ...rest } = args;
-
-  const ep = `${endpoint}${primaryKey}/`;
-  const ep2 = action ? `${endpoint}${primaryKey}/${action}/` : ep;
+  const { queryKey, primaryKey, endpoint, ...rest } = args;
 
   return useDrfQuery<Entity>({
-    queryKey: queryKey ? getEntityQueryKey(queryKey, primaryKey, action) : undefined,
-    endpoint: ep2,
+    queryKey: queryKey ? getEntityQueryKey(queryKey, primaryKey) : undefined,
+    endpoint: `${endpoint}${primaryKey}/`,
     ...rest,
   });
 }
