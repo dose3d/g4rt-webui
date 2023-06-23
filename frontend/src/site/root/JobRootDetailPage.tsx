bafe37a1aba6175278de6e8491f1276a75a394c9
Michał Niedźwiecki <nkg753@gmail.com>
2023-06-23 15:58:28 +0200

    JSROOT Browser

--- frontend/src/site/root/JobRootDetailPage.tsx
+++ frontend/src/site/root/JobRootDetailPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useJobRootFileDownload, useJobRootFileEntity } from '../../api/jobsRootFile';
 import {
@@ -11,21 +11,10 @@ import {
   Page,
 } from '../../components/layout';
 import { formatDate } from '../../utils/formatValues';
-import { draw, openFile } from 'jsroot';
+import { buildGUI } from 'jsroot';
 import { DocumentIcon } from '../../components/icons';
 import Breadcrumbs, { Breadcrumb, BreadcrumbsIconClass } from '../../components/Breadcrumbs';
 import { JobDetailPageBreadcrumbs } from '../jobs/JobDetailPage';
-
-interface FKey {
-  fClassName: string;
-  fName: string;
-  fTitle: string;
-}
-
-interface TFile {
-  fKeys: FKey[];
-  readObject: (fName: string) => Promise<any>;
-}
 
 const JobRootDetailPageBreadcrumbs = (
   jobId: number | undefined,
@@ -47,14 +36,18 @@ export default function JobRootDetailPage() {
   const { data } = useJobRootFileEntity(fId);
   const { data: rootFile, isSuccess } = useJobRootFileDownload(fId);
 
-  const [file, setFile] = useState<TFile | null>(null);
-  const [keys, setKeys] = useState<FKey[]>([]);
-
   useEffect(() => {
     if (isSuccess) {
-      openFile(rootFile).then((file: TFile) => {
-        setFile(file);
-        setKeys(file.fKeys);
+      buildGUI('jsroot').then((h: any) => {
+        document.querySelector('.jsroot_browser_title')?.remove();
+        document.querySelector('.jsroot_browser_version')?.remove();
+        document.querySelector('.gui_urlToLoad')?.remove();
+        document.querySelector('.gui_selectFileName')?.remove();
+        document.querySelector('#gui_fileCORS')?.remove();
+        document.querySelector('.gui_localFile')?.parentElement?.remove();
+        document.querySelector('.gui_ReadFileBtn')?.parentElement?.remove();
+
+        h.openRootFile(rootFile);
       });
     }
   }, [rootFile, isSuccess]);
@@ -73,30 +66,7 @@ export default function JobRootDetailPage() {
                 </div>
               </CardHeaderMain>
             </CardHeader>
-
-            <h3 className="mb-2 mt-4 font-bold">Log files:</h3>
-            <div className="ml-4">
-              <ol className="list-inside list-decimal">
-                {keys.map((o, i) => (
-                  <li key={i}>
-                    {`${o.fName}: ${o.fClassName} - ${o.fTitle}`}
-                    <button
-                      className="btn-primary btn-xs btn ml-2"
-                      onClick={() => {
-                        file!.readObject(o.fName).then((obj) => {
-                          draw('drawing', obj, '');
-                        });
-                      }}
-                    >
-                      Show
-                    </button>
-                  </li>
-                ))}
-              </ol>
-            </div>
-
-            <h3 className="mb-2 mt-4 font-bold">Details:</h3>
-            <div id="drawing" style={{ width: '100%', height: '800px' }}></div>
+            <div id="jsroot" style={{ width: '100%', height: '800px', position: 'relative' }}></div>
           </Card>
         </CardsContainer>
       </Margin>
